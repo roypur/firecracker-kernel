8b81abb1a32430017b44b45201cf1b5dd23dd50a
Roy Olav Purser <admin@royolav.net>
2020-02-25 18:34:13 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ docker build --tag firecracker-kernel-builder ${dir}
 docker run --volume "${dir}/out:/out" -it firecracker-kernel-builder
 
 cp "${dir}/out/vmlinux.bin" "${dir}/out/vmlinux-tmp.bin"
+rm "${dir}/out/vmlinux.bin"
 mv "${dir}/out/vmlinux-tmp.bin" "${dir}/out/vmlinux.bin"
