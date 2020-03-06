8280241f6b60f881226bf77a0364136d6c418266
Roy Olav Purser <admin@royolav.net>
2020-03-06 20:26:58 +0100

    kernel 5.4.24

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ dir=$(dirname $(realpath $0))
 mkdir -p "${dir}/out"
 
 docker build --tag 'firecracker-kernel-builder' "${dir}"
-id=$(docker create 'firecracker-kernel-builder')
+id=$(docker create --hostname=bullseye firecracker-kernel-builder)
 docker start --attach ${id}
 
-docker cp "${id}:/build/linux/vmlinux.bin" "${dir}/out/vmlinux.bin"
-docker rm "${id}"
+docker cp "${id}:/build/linux/vmlinux" "${dir}/out/vmlinux.bin"
+docker rm ${id}
--- build/compile.sh
+++ build/compile.sh
@@ -8,6 +8,9 @@ curl --output /build/archive/kernel.tar.xz "${KERNEL_SOURCE}"
 tar --extract --file=/build/archive/kernel.tar.xz --strip-components=1
 
 curl --silent --output /build/linux/.config 'https://raw.githubusercontent.com/firecracker-microvm/firecracker/master/resources/microvm-kernel-config'
-
 make olddefconfig
+
+sed -r -i 's/(.*)MODULE(.*)=y/\1MODULE\2=n/g' '/build/linux/.config'
+sed -r -i 's/(.*)MODVERSION(.*)=y/\1MODVERSION\2=n/g' '/build/linux/.config'
+
 make -j 8 vmlinux
